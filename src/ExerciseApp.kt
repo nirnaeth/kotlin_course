d08d6fed6ca6a5c76eea9abdf032f0a1d2592c89
Monica Giambitto <m.giambitto@freegoweb.it>
2020-12-16 13:59:43 +0100

    exercise 4 - calculate factorial

--- src/ExerciseApp.kt
+++ src/ExerciseApp.kt
@@ -1,5 +1,3 @@
-import java.lang.Exception
-
 fun main(args: Array<String>) {
     print("Give me a number:\n")
 
@@ -10,12 +8,24 @@ fun main(args: Array<String>) {
             true -> println("The number is even")
             false -> println("The number is odd")
         }
+
+        println("The factorial of $input is ${calculateFactorial(input)}")
+
     } catch (e: NumberFormatException) {
         print("The input is not a number")
     }
-
 }
 
 fun isEven(number: Int): Boolean {
     return number % 2 == 0
+}
+
+fun calculateFactorial(number: Int): Long {
+    var factorial: Long = 1
+
+    for (i in 1..number) {
+        factorial *= i
+    }
+
+    return factorial
 }
