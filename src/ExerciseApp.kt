05eafa6ac8136bf2c0d29eb4e024bc667800e116
Monica Giambitto <m.giambitto@freegoweb.it>
2020-12-16 13:35:34 +0100

    exercise 3 - even or odd with exception handling

--- src/ExerciseApp.kt
+++ src/ExerciseApp.kt
@@ -1,7 +1,21 @@
+import java.lang.Exception
+
 fun main(args: Array<String>) {
-    print("Give me a number:10\n")
+    print("Give me a number:\n")
 
-    val input: String? = readLine()
+    try {
+        val input: Int = readLine()!!.toInt()
 
-    println("The result is $input")
+        when (isEven(input)) {
+            true -> println("The number is even")
+            false -> println("The number is odd")
+        }
+    } catch (e: NumberFormatException) {
+        print("The input is not a number")
+    }
+
+}
+
+fun isEven(number: Int): Boolean {
+    return number % 2 == 0
 }
