f114f2a46aaf0f895e81297939629f49d6716abe
Monica Giambitto <m.giambitto@freegoweb.it>
2020-12-21 11:16:46 +0100

    exercise 6 - ensure single character

--- src/Text.kt
+++ src/Text.kt
@@ -1,10 +1,16 @@
 fun main() {
-    print("Give me a character:\n")
-
     val vowels = "aeiouy"
-    val input: String = readLine()!!
-    val character: Char = input.toCharArray().first()
 
+    print("Give me a character:\n")
+    var input: String = readLine().toString()
+
+    while (input.length > 1) {
+        print("Please, use only one character\n")
+        print("Give me a character:\n")
+        input = readLine().toString()
+    }
+
+    val character: Char = input.toCharArray().first()
     if (vowels.contains(character, ignoreCase = true)) {
         print("It's a vowel")
     } else {
